c072f11b2f369fd2ab1d3099e88b2c9e9bd3a8d8
Raffi Besiktasliyan <raffi.besik@hotmail.com>
2020-02-10 22:25:05 +0100

    Updates

---
+++ precache-manifest.e58456d9d864b47b4ac057b2258cdbac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b706d756c79f25052c7c1a4186b468e3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "962b933d241f1c3dad5c",
+    "url": "/monsters-rolodex/static/css/main.962b8f98.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "962b933d241f1c3dad5c",
+    "url": "/monsters-rolodex/static/js/main.7ba8216a.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
